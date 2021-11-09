bf112b53366ad9e3742cd94196b305e4f5baa668
OsmanRana <osmancmc@yahoo.com>
2021-11-10 01:46:33 +0600

    login and register setup

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,13 +1,47 @@
-import { Typography } from '@mui/material';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const Login = () => {
+    const handleOnSubmit = e => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h2>This is login</h2>
-            <NavLink to="/register" style={{textDecoration: 'none'}}><Typography  variant="h6" gutterBottom>NEW USER? PLEASE REGISTER</Typography></NavLink>
-        </div>
+        <Container sx={{ p: 0 }}>
+            <Typography sx={{ color: 'text.secondary', mt: 5, mb: 3 }} variant="h6" gutterBottom component="div">LOGIN</Typography>
+            <form
+                onSubmit={handleOnSubmit}
+            >
+                <Grid container>
+                    <Grid xs={1} md={2}>
+
+                    </Grid>
+                    <Grid xs={10} md={8}>
+                        <TextField
+                            sx={{ width: 1 }}
+                            id="outlined-basic-email"
+                            label="Your Email Please"
+                            name="email"
+                            type="email"
+                            variant="outlined" />
+                        <TextField
+                            sx={{ width: 1, my: 3 }}
+                            id="outlined-basic-password"
+                            label="Your Password Please"
+                            name="password"
+                            type="password"
+                            variant="outlined" />
+                    </Grid>
+                    <Grid xs={1} md={2} sx={{ pl: 0 }}>
+
+                    </Grid>
+                </Grid>
+                <Button type="submit" variant="contained" >Login</Button>
+            </form>
+            <NavLink to="/register" style={{ textDecoration: 'none' }}><Typography sx={{ color: 'info.main', textAlign: 'center', my: 3 }} variant="h6" gutterBottom component="div">NEW USER? PLEASE REGISTER</Typography></NavLink>
+        </Container>
     );
 };
 
--- src/Pages/Register/Register.js
+++ src/Pages/Register/Register.js
@@ -1,14 +1,56 @@
-import { Typography } from '@mui/material';
+import { Button, Container, Grid, Typography } from '@mui/material';
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 const Register = () => {
+    const handleOnSubmit = e => {
+        e.preventDefault();
+    }
     return (
-        <div>
-            <h2>This is Register</h2>
-            <NavLink to="/login" style={{textDecoration: 'none'}}><Typography variant="h6" gutterBottom>ALREADY REGISTERED? PLEASE SIGN IN</Typography></NavLink>
+        <Container sx={{ p: 0 }}>
+            <Typography sx={{ color: 'text.secondary', mt: 5, mb: 3 }} variant="h6" gutterBottom component="div">REGISTER</Typography>
+            <form
+                onSubmit={handleOnSubmit}
+            >
+                <Grid container>
+                    <Grid xs={1} md={2}>
 
-        </div>
+                    </Grid>
+                    <Grid xs={10} md={8}>
+                        <TextField
+                            sx={{ width: 1 }}
+                            id="outlined-basic-name"
+                            label="Your Full Name Please"
+                            name="name"
+                            type="name"
+                            variant="outlined" />
+                        <TextField
+                            sx={{ width: 1, mt: 3 }}
+                            id="outlined-basic-email"
+                            label="Your Email Please"
+                            name="email"
+                            type="email"
+                            variant="outlined" />
+                        <TextField
+                            sx={{ width: 1, my: 3 }}
+                            id="outlined-basic-password"
+                            label="Your Password Please"
+                            name="password"
+                            type="password"
+                            variant="outlined" />
+                    </Grid>
+                    <Grid xs={1} md={2} sx={{ pl: 0 }}>
+
+                    </Grid>
+
+                </Grid>
+                <Button type="submit" variant="contained" >Register</Button>
+
+            </form>
+            <NavLink to="/login" style={{ textDecoration: 'none' }}><Typography sx={{ color: 'info.main', textAlign: 'center', my: 3 }} variant="h6" gutterBottom component="div">ALREADY REGISTERED?<br /> PLEASE LOGIN</Typography></NavLink>
+        </Container>
     );
 };
 
